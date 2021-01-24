73571cbca114900c78fdf6fe5cd1016ed64884e7
Kahitar <kahitar.productions@gmail.com>
2021-01-24 13:50:04 +0100

    Cleanup

--- tierlist/comps/routes.py
+++ tierlist/comps/routes.py
@@ -3,6 +3,7 @@ from flask import (render_template, url_for, flash,
 from flask_login import current_user, login_required
 from tierlist import db
 from tierlist.models import Comp, Tierlist
+from tierlist.comps import utils
 from tierlist.comps.forms import CompForm
 from tierlist.tierlist.utils import update_tierlist
 
@@ -56,98 +57,19 @@ def update_comp(comp_id):
                            form=form, legend="Update Comp")
 
 
-def get_max_subtier(comps):
-    print("Same tier: ", comps)
-
-    max_subtier = 0
-    for comp in comps:
-        if comp.sub_tier > max_subtier:
-            max_subtier = comp.sub_tier
-    return max_subtier
-
-
-def get_same_tier_comps(comp, all_comps):
-    same_tier = [c for c in all_comps if c.tier == comp.tier and c is not comp]
-    same_tier.sort(key=lambda c: c.sub_tier)
-    return same_tier
-
-
-def tier_up(comp, all_comps):
-    old_tier_comps = get_same_tier_comps(comp, all_comps)
-    old_sub_tier = comp.sub_tier
-    comp.tier -= 1
-    new_tier_comps = get_same_tier_comps(comp, all_comps)
-
-    # Fix sub-tiers of old tier
-    for c in old_tier_comps:
-        if c.sub_tier > old_sub_tier:
-            c.sub_tier -= 1
-
-    if comp.tier <= 0:
-        tier_down(comp, all_comps)
-    else:
-        max_subtier = get_max_subtier(new_tier_comps)
-        comp.sub_tier = max_subtier + 1
-
-
-def tier_down(comp, all_comps):
-    old_tier_comps = get_same_tier_comps(comp, all_comps)
-    old_sub_tier = comp.sub_tier
-    comp.tier += 1
-    comp.sub_tier = 1
-    new_tier_comps = get_same_tier_comps(comp, all_comps)
-
-    # Fix sub-tiers of old tier
-    for c in old_tier_comps:
-        if c.sub_tier > old_sub_tier:
-            c.sub_tier -= 1
-
-    for c in new_tier_comps:
-        c.sub_tier += 1
-
-
-def sub_tier_up(comp, all_comps):
-    tier_comps = get_same_tier_comps(comp, all_comps)  # not including comp
-
-    if comp.sub_tier <= 1:
-        tier_up(comp, all_comps)
-    else:
-        comp.sub_tier -= 1
-
-        # Fix comp's sub-tiers
-        for c in tier_comps:
-            if c.sub_tier == comp.sub_tier:
-                c.sub_tier += 1
-
-
-def sub_tier_down(comp, all_comps):
-    tier_comps = get_same_tier_comps(comp, all_comps)  # not including comp
-    max_sub_tier = get_max_subtier(tier_comps)  # not including comp
-
-    if comp.sub_tier > max_sub_tier:
-        tier_down(comp, all_comps)
-    else:
-        comp.sub_tier += 1
-
-        # Fix comp's sub-tiers
-        for c in tier_comps:
-            if c.sub_tier == comp.sub_tier:
-                c.sub_tier -= 1
-
-
 @comps.route("/comp/<int:comp_id>/<string:direction>/move")
 def move_comp(comp_id, direction):
     comp = Comp.query.get_or_404(comp_id)
     all_comps = Comp.query.filter_by(tierlist=comp.tierlist).all()
 
     if direction == 'tier-up':
-        tier_up(comp, all_comps)
+        utils.tier_up(comp, all_comps)
     if direction == 'tier-down':
-        tier_down(comp, all_comps)
+        utils.tier_down(comp, all_comps)
     if direction == 'up':
-        sub_tier_up(comp, all_comps)
+        utils.sub_tier_up(comp, all_comps)
     if direction == 'down':
-        sub_tier_down(comp, all_comps)
+        utils.sub_tier_down(comp, all_comps)
 
     db.session.commit()
 
---
+++ tierlist/comps/utils.py
@@ -0,0 +1,78 @@
+
+def get_max_subtier(comps):
+    print("Same tier: ", comps)
+
+    max_subtier = 0
+    for comp in comps:
+        if comp.sub_tier > max_subtier:
+            max_subtier = comp.sub_tier
+    return max_subtier
+
+
+def get_same_tier_comps(comp, all_comps):
+    same_tier = [c for c in all_comps if c.tier == comp.tier and c is not comp]
+    same_tier.sort(key=lambda c: c.sub_tier)
+    return same_tier
+
+
+def tier_up(comp, all_comps):
+    old_tier_comps = get_same_tier_comps(comp, all_comps)
+    old_sub_tier = comp.sub_tier
+    comp.tier -= 1
+    new_tier_comps = get_same_tier_comps(comp, all_comps)
+
+    # Fix sub-tiers of old tier
+    for c in old_tier_comps:
+        if c.sub_tier > old_sub_tier:
+            c.sub_tier -= 1
+
+    if comp.tier <= 0:
+        tier_down(comp, all_comps)
+    else:
+        max_subtier = get_max_subtier(new_tier_comps)
+        comp.sub_tier = max_subtier + 1
+
+
+def tier_down(comp, all_comps):
+    old_tier_comps = get_same_tier_comps(comp, all_comps)
+    old_sub_tier = comp.sub_tier
+    comp.tier += 1
+    comp.sub_tier = 1
+    new_tier_comps = get_same_tier_comps(comp, all_comps)
+
+    # Fix sub-tiers of old tier
+    for c in old_tier_comps:
+        if c.sub_tier > old_sub_tier:
+            c.sub_tier -= 1
+
+    for c in new_tier_comps:
+        c.sub_tier += 1
+
+
+def sub_tier_up(comp, all_comps):
+    tier_comps = get_same_tier_comps(comp, all_comps)  # not including comp
+
+    if comp.sub_tier <= 1:
+        tier_up(comp, all_comps)
+    else:
+        comp.sub_tier -= 1
+
+        # Fix comp's sub-tiers
+        for c in tier_comps:
+            if c.sub_tier == comp.sub_tier:
+                c.sub_tier += 1
+
+
+def sub_tier_down(comp, all_comps):
+    tier_comps = get_same_tier_comps(comp, all_comps)  # not including comp
+    max_sub_tier = get_max_subtier(tier_comps)  # not including comp
+
+    if comp.sub_tier > max_sub_tier:
+        tier_down(comp, all_comps)
+    else:
+        comp.sub_tier += 1
+
+        # Fix comp's sub-tiers
+        for c in tier_comps:
+            if c.sub_tier == comp.sub_tier:
+                c.sub_tier -= 1
