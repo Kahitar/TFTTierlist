6a76852a67405d09d679ee76acd1d39451d9a4da
Kahitar <kahitar.productions@gmail.com>
2021-01-25 12:16:19 +0100

    Don't paginate posts on homepage

--- tierlist/main/routes.py
+++ tierlist/main/routes.py
@@ -11,9 +11,7 @@ def home():
     comps = Comp.query.order_by(Comp.tier.asc(), Comp.sub_tier.asc()).all()
     tierlist = Tierlist.query.first()
 
-    post_page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(
-        Post.date_posted.desc()).paginate(page=post_page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).all()
 
     return render_template('tierlist.html', comps=comps, tierlist=tierlist, posts=posts, title="Sologesang")
 
