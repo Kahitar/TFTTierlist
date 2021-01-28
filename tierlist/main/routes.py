7b47dea70da18efaae0b596c23060b4fadff56c7
Kahitar <kahitar.productions@gmail.com>
2021-01-28 11:38:23 +0100

    Show all public tierlists on home route

--- tierlist/main/routes.py
+++ tierlist/main/routes.py
@@ -6,18 +6,23 @@ main = Blueprint('main', __name__)
 
 
 @main.route("/")
+@main.route("/<int:active_tierlist_id>")
 @main.route("/home")
-def home():
-    tierlist = Tierlist.query.filter_by(is_public=1).first()
-    if tierlist is None:
-        # If there's no main tierlist, just get the first result
-        tierlist = Tierlist.query.first()
-    comps = Comp.query.filter_by(tierlist=tierlist).order_by(
-        Comp.tier.asc(), Comp.sub_tier.asc()).all()
+@main.route("/home/<int:active_tierlist_id>")
+def home(active_tierlist_id=None):
+    tierlists = Tierlist.query.filter_by(is_public=1).all()
+    all_comps = []
+    for t_list in tierlists:
+        all_comps.append(Comp.query.filter_by(tierlist=t_list).order_by(
+            Comp.tier.asc(), Comp.sub_tier.asc()).all())
+    if active_tierlist_id:
+        active_tierlist = Tierlist.query.get_or_404(active_tierlist_id)
+    else:
+        active_tierlist = None
 
     posts = Post.query.order_by(Post.date_posted.desc()).all()
 
-    return render_template('home.html', comps=comps, tierlist=tierlist, posts=posts, title="Sologesang")
+    return render_template('home.html', tierlists=tierlists, all_comps=all_comps, active_tierlist=active_tierlist, posts=posts, title="Sologesang")
 
 
 @main.route("/about")
