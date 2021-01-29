f15058f1062436c964e029fd0b88db762f93de68
Kahitar <kahitar.productions@gmail.com>
2021-01-29 16:04:55 +0100

    Fix: Ignore non-existing picture for posts

--- tierlist/posts/utils.py
+++ tierlist/posts/utils.py
@@ -20,4 +20,5 @@ def save_picture(form_picture):
 def delete_picture(picture_filename):
     picture_path = os.path.join(
         current_app.root_path, 'static/post_pics', picture_filename)
-    os.remove(picture_path)
+    if os.path.isfile(picture_path):
+        os.remove(picture_path)
