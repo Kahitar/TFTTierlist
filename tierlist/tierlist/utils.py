b850a1d856cb9cb686609d53ac564117d9e388dc
Kahitar <kahitar.productions@gmail.com>
2022-06-11 14:20:09 +0200

    fix: zero-padded subtier string for sorting tierlist

--- tierlist/tierlist/utils.py
+++ tierlist/tierlist/utils.py
@@ -11,7 +11,7 @@ def update_tierlist(tierlist):
 
 def fix_subtier_gaps(tierlist):
     all_comps = Comp.query.filter_by(tierlist=tierlist).all()
-    all_comps.sort(key=lambda c: float(f"{c.tier}.{c.sub_tier}"))
+    all_comps.sort(key=lambda c: float(f"{c.tier}.{c.sub_tier:05}"))
 
     next_subtier = 1
     current_tier = 0
